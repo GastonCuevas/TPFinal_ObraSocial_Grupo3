b015920bc5abfd75610d83064257ed8b79dcaa0c
GastonCuevas <gas_cuevas@hotmail.com>
2020-06-28 18:33:15 -0300

    Segundo commit, añadí primera versión del frontend... Gastón Cuevas

---
+++ proyecto_angular_tp3-master/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule }    from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxDataTableModule} from "angular-9-datatable";
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { AbouthUsComponent } from './components/abouth-us/abouth-us.component';
+ 
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    FooterComponent,
+    AbouthUsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgxDataTableModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
