03437ef0e91bf5477ac0c677841a17bd94b554e8
Brunoich <51252229+Brunoich@users.noreply.github.com>
2020-07-07 18:42:25 -0300

    Mejoras Visuales / "nosotros" / contactenos
    Se implementaron mejoras visuales, se realizo el contenido del enlace "nosotros" y se implemento la vista "contactenos"

--- proyecto_angular_tp3-master/src/app/components/abouth-us/abouth-us.component.ts
+++ proyecto_angular_tp3-master/src/app/components/abouth-us/abouth-us.component.ts
@@ -7,7 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AbouthUsComponent implements OnInit {
 
+  mostrarsobrenosotros: boolean=true;
+  mostrarcontactenos: boolean=false;
+  mostrardelegaciones: boolean=false;
+  mostrarbeneficiarios: boolean=false;
+
   constructor() { }
+
+  public verSobrenosotros(){
+    this.mostrarsobrenosotros=true;
+    this.mostrarcontactenos=false;
+    this.mostrardelegaciones=false;
+    this.mostrarbeneficiarios=false;
+  }
+  public verContactenos(){
+    this.mostrarsobrenosotros=false;
+    this.mostrarcontactenos=true;
+    this.mostrardelegaciones=false;
+    this.mostrarbeneficiarios=false;
+  }
+  public verDelegaciones(){
+    this.mostrarsobrenosotros=false;
+    this.mostrarcontactenos=false;
+    this.mostrardelegaciones=true;
+    this.mostrarbeneficiarios=false;
+  }
+  public verBeneficiarios(){
+    this.mostrarsobrenosotros=false;
+    this.mostrarcontactenos=false;
+    this.mostrardelegaciones=false;
+    this.mostrarbeneficiarios=true;
+  }
 
   ngOnInit(): void {
   }
